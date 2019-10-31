47d7827b616d8ffc15d012ca4253a4731b4012cc
RenownedOyster <anthonywbutler1@gmail.com>
2019-10-31 10:34:20 +0000

    Add no comment file

---
+++ aggregateLogicLayerNoComments.js
@@ -0,0 +1,38 @@
+const dummyData = [
+  {
+    name: 'John McBally',
+    gamesPerSeason: 122,
+    averageHitsPerGame: 22,
+    fartsPerMinute: 10
+  },
+    {
+    name: 'Bob McBaseballbat',
+    gamesPerSeason: 122,
+    averageHitsPerGame: 22,
+    fartsPerMinute: 10
+  },
+    {
+    name: 'Alfonse McStrikey',
+    gamesPerSeason: 122,
+    averageHitsPerGame: 22,
+    fartsPerMinute: 10
+  },
+];
+
+const getData = (fieldName, individualData) => [fieldName]individualData;
+const mapThroughApiResponse = (fieldName, getterFunction, apiResponse) => apiResponse.map(individualData => getterFunction(fieldName, individualData));
+const mapThroughAndAverage = newDataCollection => {
+  const totalOfAllValues = newDataCollection.reduce(
+    (accumulator, individualStatistic) => {
+    accumulator + individualStatistic
+    }, 0);
+  const dataLength = newDataCollection.length;
+
+  return totalOfAllValues / dataLength;
+}
+
+console.log(
+  mapThroughAndAverage(
+    mapThroughApiResponse('gamesPerSeason', getData(), dummyData)
+    )
+);
